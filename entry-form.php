dd1c265908dfbce80bd8659e13f104f2d3330697
Rebecca Weber <rebecca@aquilinoarts.com>
2015-11-10 11:40:07 -0500

    deleting unused files

--- entry-form.php
+++
@@ -1,8 +0,0 @@
-<form method="post" class="entry-form">
-<label>First Name: <input type="text" required id="first" name="first" placeholder="first name"></label>
-<label>Last Name: <input type="text" required id="last" name="last" placeholder="last name"></label>
-<label>Company/Organization: <input type="text" id="org" name="org" placeholder="company/organization"></label>
-<label>Email: <input type="email" required id="email" name="email" placeholder="email address"></label>
-<label>Cell Phone Number (optional): <input type="tel" id="cell" name="cell"></label>
-<button id="enter-button" class="enter-button">Enter Drawing</button>
-</form>

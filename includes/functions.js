554dbce274ec495c93885d028a916147b16c0ed0
Rebecca Weber <rebecca@aquilinoarts.com>
2015-11-10 11:41:04 -0500

    deleting unused files

--- includes/functions.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    if (!($('#entry-name').length)) {
-        setInterval(function () {
-            $('#form-itself').load('entry-form.html');
-        }, 2500);
-    }
-
-});
